5ff9ae8bba71981d29890d2b693213178f176fa1
John Lloyd Navaja <johnlloydnavaja2@gmail.com>
2025-08-14 22:13:50 +0800

    first commit

---
+++ activity3.py
@@ -0,0 +1,3 @@
+sprint("magandang hapon", \n\n\t\t\tBSIT1B")
+Sprint("the quick brown" \fjumps over the lazy\b\b\b dog.!")
+print("ikaw lang sapat na\""
---
+++ code_challenge2.py
@@ -0,0 +1,14 @@
+n1 = eval(input("Enter the amount of deposit 9973"))
+n2 = eval(input("This is to breakdown the amout of the deposit "))
+print("1000 to breakdown "
+s = n1 + n2
+d = n1 - n2
+p = n1 * n2
+q = n1 / n2
+print("\nThe sum of",n1,"and",n2,"is",s) # backlash code is support by Python. \n
+print("The difference of",n1,"and",n2,"is",d)
+print("The product of",n1,"and",n2,"is",p)
+print("The quotien of",n1,"and",n2,"is",q)
+print(n1, "exponent by",n2,"is",n1**n2)
+print("The remainder of",n1,"and",n2,"is",n1 % n2)
+print("The floor division of",n1,"and",n2,"is",n1//n2)

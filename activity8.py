4259beff8b733afd9b991c6cf0cf8dd29d752759
John Lloyd Navaja <johnlloydnavaja2@gmail.com>
2025-08-22 19:15:58 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,26 @@
+y = 7
+z = 4
+
+c = 5
+d = 8
+
+print ( y > z )
+print ( c < d )
+name1 = 'Tumboy'
+name2 = 'Yobmut'
+
+print ( (name1 != name2) )
+
+y = 7
+z = 4
+
+c = 5
+d = 8
+
+print ( y < z )
+print ( c > d )
+
+name1 = 'Tumboy'
+name2 = 'Tumboy'
+
+print ( ( name1 > name2) )

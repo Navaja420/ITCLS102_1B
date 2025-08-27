d643b707f2f57c98765747e20194da2dc2dd7018
John Lloyd Navaja <johnlloydnavaja2@gmail.com>
2025-08-27 15:15:46 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,11 @@
+username = "malas_ak"
+password = "mangangain" 
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password):
+   print("ACCESS GRANTED")
+
+else:
+   print("ACCESS DENIED")

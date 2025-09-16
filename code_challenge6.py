9173f2dcf7f593b688fd5836fe94b58835f1b7a9
John Lloyd Navaja <johnlloydnavaja2@gmail.com>
2025-09-16 23:43:35 +0800

    Update code_challenge6.py

--- code_challenge6.py
+++ code_challenge6.py
@@ -1,8 +1,8 @@
 print("Welcome to the odd number summation program!")
 
 total = 0
-for i in range(7):
+for odd in range(7):
     num = int(input("Enter a number:  "))
-    if num % 2 != 0:
+    if (num % 2 = 0):
         total += num
 print("Sum of odd numbers entered:", total)

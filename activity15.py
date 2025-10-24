96ce126206b69f3ed5f5e9b6a971304c33655698
John Lloyd Navaja <johnlloydnavaja2@gmail.com>
2025-10-24 10:05:29 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,8 @@
+fname = 'John Lloyd'
+mname = 'Collantes'
+lname = 'Navaja'
+house = 'Python'
+sy = 2024 - 2025
+fullname = f"{fname} {mname} {lname}"
+
+print(f"My name is {fullname}, and from the house of {house} sy:{sy}")
